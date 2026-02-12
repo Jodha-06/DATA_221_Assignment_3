b67c548f297082d17fd8e5668360617116183b09
Jodha-06 <Jodhveerd06@gmail.com>
2026-02-12 11:32:39 -0700

    Finish Question_4.py for Assignment 3

---
+++ Question_4.py
@@ -0,0 +1,34 @@
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+import pandas as pd
+
+kidney_disease_dataFrame = pd.read_csv("kidney_disease.csv")
+
+featureMatrix = kidney_disease_dataFrame._get_numeric_data()
+targetLabels = kidney_disease_dataFrame["classification"].map({"ckd":1,"notckd":0})
+
+featureMatrix = featureMatrix.dropna()
+targetLabels = targetLabels.loc[featureMatrix.index]
+
+features_train, features_test, labels_train, labels_test = train_test_split(featureMatrix,targetLabels,test_size=0.3,random_state=42)
+
+knnModel = KNeighborsClassifier(n_neighbors=5)
+knnModel.fit(features_train,labels_train)
+
+predictedLabels = knnModel.predict(features_test)
+
+print("Confusion Matrix:\n", confusion_matrix(labels_test,predictedLabels))
+print("Accuracy:", accuracy_score(labels_test,predictedLabels))
+print("Precision:", precision_score(labels_test,predictedLabels))
+print("Recall:", recall_score(labels_test,predictedLabels ))
+print("F1-score:", f1_score(labels_test,predictedLabels ))
+
+# In kidney disease prediction, a True positive means that the model correctly predicts that a patient has kidney disease whilst a True negative means that the model correctly predicts that a patient is healthy.
+#Conversely, a False positive means that the model predicts kidney disease for a healthy patient whilst a False negative means that the model fails to detect kidney disease in a patient that has it.
+# Accuracy alone is not enough because a model can appear accurate but can be missing important disease cases, which may be a result of missing or imbalanced data
+# The metric that is the most important in the case of missing a kidney disease that is very serious is recall, as this measures how well the model identifies patients who actually have kidney disease
